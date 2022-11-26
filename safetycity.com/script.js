4270f86d028271b63179447e4744e4922e778b01
alexeus8776 <106432402+alexeus8776@users.noreply.github.com>
2022-11-26 14:27:12 +0900

    Update script.js

--- safetycity.com/script.js
+++ safetycity.com/script.js
@@ -4,7 +4,7 @@ function init() {
     var myPlacemark,
         myMap = new ymaps.Map('map', {
             center: [62.027221,129.732178],
-            zoom: 13,
+            zoom: 14,
 			controls: []
         }, {
             searchControlProvider: 'yandex#search'
@@ -33,7 +33,7 @@ function init() {
 						<li>Фото: <br /> <input type="file" name="foto" /></li>\
                     </ul>\
                     <div align="center">
-                    <button id="1" name='knopka'type="submit"style='background:lightgreen;padding:4px 20px 4px 20px; fomt-size:20px; border:2px solid green;' />Сохранить</button>
+                    <input id="1" name='knopka'type="submit"style='background:lightgreen;padding:4px 20px 4px 20px; fomt-size:20px; border:2px solid green;' value='Сохранить'>
                 </div>'
 				`
 				
@@ -45,7 +45,54 @@ function init() {
         myPlacemark = createPlacemark(coords);
         myMap.geoObjects.add(myPlacemark);
     });
-
+    var myPlacemark = new ymaps.Placemark([62.036357912995655,129.74593016711418], {
+        balloonContentHeader: "Короленко 6",
+        balloonContentBody: "Магазин 'Виктория'",
+        balloonContentFooter: "Не работает терминал"
+}, {
+preset: 'islands#violetDotIconWithCaption'
+});
+var myPlacemark1 = new ymaps.Placemark([62.02342159823361,129.73241775366503], {
+        balloonContentHeader: "Титовская 6",
+        balloonContentBody: "Магазин 'У коли'",
+        balloonContentFooter: "Картой не принимают"
+}, {
+preset: 'islands#violetDotIconWithCaption'
+});
+var myPlacemark2 = new ymaps.Placemark([62.028246416317316,129.7480772526963], {
+        balloonContentHeader: "Мамаева 3",
+        balloonContentBody: "Магазин 'Топ'",
+        balloonContentFooter: "Не терминал работает "
+}, {
+preset: 'islands#violetDotIconWithCaption'
+});
+var myPlacemark3 = new ymaps.Placemark([62.031068033547776,129.75230890557089], {
+        balloonContentHeader: "Петровского 63",
+        balloonContentBody: "Магазин 'Лада'",
+        balloonContentFooter: "Нет терминала"
+}, {
+preset: 'islands#violetDotIconWithCaption'
+});
+var myPlacemark4 = new ymaps.Placemark([62.03311550823438,129.73986481456117], {
+        balloonContentHeader: "Автодорожная 29",
+        balloonContentBody: "Магазин 'История'",
+        balloonContentFooter: "Нету"
+}, {
+preset: 'islands#violetDotIconWithCaption'
+});
+var myPlacemark5 = new ymaps.Placemark([62.030203574435134,129.72189566766318], {
+        balloonContentHeader: "Дзержинского 56",
+        balloonContentBody: "Магазин 'Фора'",
+        balloonContentFooter: "Не работает"
+}, {
+preset: 'islands#violetDotIconWithCaption'
+});
+myMap.geoObjects.add(myPlacemark);
+myMap.geoObjects.add(myPlacemark1);
+myMap.geoObjects.add(myPlacemark2);
+myMap.geoObjects.add(myPlacemark3);
+myMap.geoObjects.add(myPlacemark4);
+myMap.geoObjects.add(myPlacemark5);
     
 
 }
